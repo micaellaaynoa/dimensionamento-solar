e9eae1b939eafd3d96733e7c6683f23ffd707da7
GCastilho <castilho.gabriel14@gmail.com>
2020-04-24 10:01:06 -0300

    Adicionado um mapa clicável
    -- Adicionado uma rota /app para ser a rota (temporária) do app de dimensionamento
    -- Adicionado um mapa do brasil clicável em svg, que dispara um evento 'click' com o nome do estado clicado
    -- Minor changes

---
+++ index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Mapa from './_mapa.svelte'
+
+	function printClick(event) {
+		console.log(event.detail)
+	}
+</script>
+
+<h2>Hello from app</h2>
+
+<Mapa on:click="{printClick}" />
